7a257e8cfb9cfeec24bdc8d5c56016e94da91ae9
Gavin Gilmour <gavin.gilmour@scotland.pnn.police.uk>
2017-11-10 14:26:27 +0000

    rework load

--- util/load.js
+++ util/load.js
@@ -1,5 +1,5 @@
 const glob = require("glob");
-const request = require("request");
+const request = require("request-promise");
 const base64 = require("file-base64");
 const path = require("path");
 const chalk = require("chalk");
@@ -23,98 +23,82 @@ request.debug = false; // Enable this for DD request debugging
     LOAD_INPUT_DIRECTORY
   } = require("./config.json");
 
-  console.log(chalk.blue("Deleting Index"));
-  request.del(LOAD_ES_API, {}, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    }
-  });
+  try {
+    console.log(chalk.blue("Deleting Index.."));
+    await request.del(LOAD_ES_API);
+  } catch (error) {
+    console.log(error);
+  }
 
   console.log(chalk.blue("Creating ES mapping.."));
-  request.put(
-    {
-      url: LOAD_ES_API,
-      json: {
-        mappings: {
-          img: {
-            properties: {
-              doc: {
-                properties: {
-                  categories: {
-                    properties: {
-                      category: {
-                        fields: {
-                          keyword: { ignore_above: 256, type: "keyword" },
-                          raw: { type: "string", index: "not_analyzed" }
-                        },
-                        type: "text"
+  await request.put({
+    url: LOAD_ES_API,
+    json: {
+      mappings: {
+        img: {
+          properties: {
+            doc: {
+              properties: {
+                categories: {
+                  properties: {
+                    category: {
+                      fields: {
+                        keyword: { ignore_above: 256, type: "keyword" },
+                        raw: { type: "string", index: "not_analyzed" }
                       },
-                      score: { type: "float" }
-                    }
-                  },
-                  sourceKey: {
-                    fields: { keyword: { ignore_above: 256, type: "keyword" } },
-                    type: "text"
+                      type: "text"
+                    },
+                    score: { type: "float" }
                   }
+                },
+                sourceKey: {
+                  fields: { keyword: { ignore_above: 256, type: "keyword" } },
+                  type: "text"
                 }
               }
             }
           }
         }
       }
-    },
-    (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
     }
-  );
+  });
 
   console.log(chalk.green("Importing images.."));
-  glob(`${LOAD_INPUT_DIRECTORY}/**/*.jpg`, (er, files) => {
-    files.forEach(file => {
+  glob(`${LOAD_INPUT_DIRECTORY}/**/*.jpg`, async (er, files) => {
+    for (let file of files) {
       const fileName = path.basename(file, path.extname(file));
-      base64.encode(file, (err, data) => {
-        request.post(
-          {
-            url: `${LOAD_DD_API}/_bulk`,
-            json: {
-              service: "face",
-              parameters: {
-                mllib: {
-                  gpu: true
-                },
-                input: {
-                  width: 224,
-                  height: 224
-                },
-                output: {
-                  best: 10,
-                  template:
-                    '{{#body}} {{#predictions}} { "index": {"_index": "images", "_type":"img" } }\n {"doc": { "sourceKey":"' +
-                    fileName +
-                    '","categories": [ {{#classes}} { "category":"{{cat}}","score":{{prob}} } {{^last}},{{/last}}{{/classes}} ] } }\n {{/predictions}} {{/body}} }',
-                  network: {
-                    url: `${LOAD_ES_API}/_bulk`,
-                    http_method: "POST"
-                  }
-                }
+      base64.encode(file, async (err, data) => {
+        console.log(chalk.red(`${fileName}`));
+        const response = await request.post({
+          url: `${LOAD_DD_API}/_bulk`,
+          json: {
+            service: "face",
+            parameters: {
+              mllib: {
+                gpu: true
               },
-              data: [data]
-            }
-          },
-          (error, response, body) => {
-            if (error) {
-              console.log("statusCode:", response && response.statusCode);
-              console.log(error);
-            }
-            if (body && "items" in body) {
-              console.log(chalk.blue("Submitted"));
-              console.log(chalk.green(util.inspect(body.items, false, null)));
-            }
+              input: {
+                width: 224,
+                height: 224
+              },
+              output: {
+                best: 10,
+                template:
+                  '{{#body}} {{#predictions}} { "index": {"_index": "images", "_type":"img" } }\n {"doc": { "sourceKey":"' +
+                  fileName +
+                  '","categories": [ {{#classes}} { "category":"{{cat}}","score":{{prob}} } {{^last}},{{/last}}{{/classes}} ] } }\n {{/predictions}} {{/body}} }',
+                network: {
+                  url: `${LOAD_ES_API}/_bulk`,
+                  http_method: "POST"
+                }
+              }
+            },
+            data: [data]
           }
-        );
+        });
+        console.log(chalk.blue("Submitted"));
+        console.log(chalk.green(util.inspect(response.items, false, null)));
       });
-    });
+    }
   });
 })();
